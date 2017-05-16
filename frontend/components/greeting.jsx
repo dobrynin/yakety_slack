c3570d816c3ad5a5585f984638942fe60ac06272
App Academy Student <student@appacademy.io>
2017-05-15 20:59:37 -0700

    Creates Greeting component

---
+++ frontend/components/greeting.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const sessionLinks = () => (
+  <nav className="login-signup">
+    <Link to="/login">Login</Link>
+    &nbsp;or&nbsp;
+    <Link to="/signup">Sign Up</Link>
+  </nav>
+);
+
+const personalGreeting = (currentUser, logout) => (
+  <div>
+    <h1 className="header-team-name"></h1>
+    <button className='header-button' onClick={logout}>Log Out</button>
+  </div>
+);
+
+const Greeting = ({ currentUser, logout }) => (
+  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
+);
