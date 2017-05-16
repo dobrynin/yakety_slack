907e4435923dcb69853e286cce0a7e2cd327440a
App Academy Student <student@appacademy.io>
2017-05-15 19:13:48 -0700

    Creates Auth Backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :destroy]
+  end
+
+  namespace :api do
+    resources :users, only: [:create]
+  end
+
   root 'static_pages#root'
 end
