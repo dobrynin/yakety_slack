b53d118a75b0687ffe03728f2abd173b2124a534
App Academy Student <student@appacademy.io>
2017-05-15 19:29:38 -0700

    Creates Session API Utility file (SessionApiUtil)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/slack_clone.jsx',
+  entry: './frontend/yak.jsx',
   output: {
       path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
       filename: "bundle.js"
