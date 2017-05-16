8444a9c8d84e365ac33fa1f9de0b8f1b799b97ec
App Academy Student <student@appacademy.io>
2017-05-15 20:08:49 -0700

    Updates the Entry Point to render the Root component in the #root container with the store passed in as a prop

--- frontend/yak.jsx
+++ frontend/yak.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { signup, login, logout } from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
 
-window.signup = signup;
-window.login = login;
-window.logout = logout;
-window.store = configureStore();
-
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Yak!</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
