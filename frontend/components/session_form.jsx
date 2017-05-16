1d6393a61385cb88f64c26818ba85224f5affbf9
App Academy Student <student@appacademy.io>
2017-05-15 21:34:20 -0700

    Creates Session Form component

---
+++ frontend/components/session_form.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn) {
+      this.props.history.push('/');
+    }
+  }
+
+  update (field) {
+    return e => this.setState({ [field]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  navLink() {
+    if (this.props.formType === 'login') {
+      return <Link to="signup">Sign up instead</Link>;
+    } else {
+      return <Link to="/login">Log in instead</Link>;
+    }
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div classNmae='session-form-container'>
+        <form onSubmit={this.handleSubmit} className='session-form-box'>
+          Welcome to Yak!
+          <br/>
+          Please {this.props.formType} or {this.navLink()}
+          {this.renderErrors()}
+          <div className="session-form">
+            <label>Username:
+              <input type="text"
+                value={this.state.username}
+                onChange={this.update('username')}
+                className='session-input'
+              />
+            </label>
+            <label>
+              <input type="password"
+              value={this.state.password}
+              onChange={this.update('password')}
+              className='session-input'
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SessionForm);
