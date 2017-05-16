c300a778056abd8d2abf02ea30f661b0ef3eaa69
App Academy Student <student@appacademy.io>
2017-05-15 21:56:02 -0700

    Creates a preloaded state with the current user, if there is one, to feed into the store when it is created

--- frontend/yak.jsx
+++ frontend/yak.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import { login } from './actions/session_actions';
-window.login = login;
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
