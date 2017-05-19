9131085a01c689f44ae40e271bf9cf7424ef67ff
App Academy Student <student@appacademy.io>
2017-05-19 14:54:33 -0700

    Updates User database schema to have column for avatar url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518163851) do
+ActiveRecord::Schema.define(version: 20170519215252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170518163851) do
     t.string   "session_token",   null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "avatar_url"
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
