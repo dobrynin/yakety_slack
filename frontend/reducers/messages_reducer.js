3f76963e7bd20ab9d5607e1215480fdc46bde6d5
App Academy Student <student@appacademy.io>
2017-05-19 11:52:58 -0700

    updates message reducer

--- frontend/reducers/messages_reducer.js
+++ frontend/reducers/messages_reducer.js
@@ -4,11 +4,18 @@ import {
   RECEIVE_CHANNEL_DATA
 } from '../actions/channel_actions';
 
+import {
+  RECEIVE_MESSAGE
+} from '../actions/message_actions';
+
 const MessagesReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_CHANNEL_DATA:
       return action.messages || {};
+    case RECEIVE_MESSAGE:
+      const newMessage = {[action.message.id]: action.message};
+      return merge({}, state, newMessage);
     default:
       return state;
   }
