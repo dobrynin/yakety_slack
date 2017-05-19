2253ab7ec509ebdc2bda6e9b8b1b97ef23e0f916
App Academy Student <student@appacademy.io>
2017-05-18 18:17:09 -0700

    More Channel styling

--- frontend/components/home_page/message_index_header.jsx
+++ frontend/components/home_page/message_index_header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const MessageIndexHeader = ({ channelName, userCount, channelDescription }) => (
   <div className='message-index-header'>
     <h2 className='message-index-header-channel-name'>{ channelName }</h2>
@@ -7,10 +8,13 @@ const MessageIndexHeader = ({ channelName, userCount, channelDescription }) => (
       <ul className='message-index-header-info'>
         <li className='member-count'>
           <i className="fa fa-user" aria-hidden="true"></i>
-          { userCount }
+          &nbsp; { userCount } &nbsp;
+        </li>
+        <li className='divider-bar'>
+          |
         </li>
         <li className='channel-description'>
-          { channelDescription }
+          &nbsp; { channelDescription }
         </li>
       </ul>
     </div>
