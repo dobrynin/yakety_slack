bdf3a583fc17b1a4389dfb18325d890ef255de0d
App Academy Student <student@appacademy.io>
2017-05-19 15:28:45 -0700

    message styling

--- frontend/components/home_page/message_index_item.jsx
+++ frontend/components/home_page/message_index_item.jsx
@@ -5,8 +5,8 @@ const MessageIndexItem = ({ message, user }) => (
       <img className='avatar' src={user.avatar_url} />
     </div>
     <div className='message-index-item-body'>
-      <h4>{ user.username }</h4>
-      <p>{ message.body }</p>
+      <h4 className='message-username'>{ user.username }</h4>
+      <p className='message-body'>{ message.body }</p>
     </div>
   </div>
 );
