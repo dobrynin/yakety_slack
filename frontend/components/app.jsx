44f388dae8881b1eac519f9ddfe2bcc605f6aaa2
App Academy Student <student@appacademy.io>
2017-05-20 18:13:56 -0700

    Creates basic modal based on React documentation example

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,14 +6,77 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SplashPage from './splash_page/splash_page';
 import SessionPage from './session_form/session_page';
 import HomePage from './home_page/home_page';
+import Modal from 'react-modal';
+import ReactDOM from 'react-dom';
 
-const App = () => (
-  <div>
-    <AuthRoute path='/login' component={SessionPage} />
-    <AuthRoute path='/signup' component={SessionPage} />
-    <Route exact path='/' component={SplashPage} />
-    <ProtectedRoute path='/channels' component={HomePage} />
-  </div>
-);
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    const appElement = document.getElementById('root');
+    Modal.setAppElement(appElement);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.openModal}>Open Modal</button>
+         <Modal
+           isOpen={this.state.modalIsOpen}
+           onAfterOpen={this.afterOpenModal}
+           onRequestClose={this.closeModal}
+           style={customStyles}
+           contentLabel="Example Modal"
+         >
+          <h2 ref={this.subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+        <AuthRoute path='/login' component={SessionPage} />
+        <AuthRoute path='/signup' component={SessionPage} />
+        <Route exact path='/' component={SplashPage} />
+        <ProtectedRoute path='/channels' component={HomePage} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+const appElement = document.getElementById('root');
+Modal.setAppElement(appElement);
 
 const Root = ({ store }) => (
   <Provider store={ store }>
