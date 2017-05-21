84887fa986f601195a2ebb129fa30775fe35656b
App Academy Student <student@appacademy.io>
2017-05-20 18:35:26 -0700

    adds modal to channel index

--- frontend/components/home_page/channel_index.jsx
+++ frontend/components/home_page/channel_index.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 import ChannelIndexItem from './channel_index_item';
 import { selectChannels, selectDMs } from '../../reducers/selectors';
+import Modal from 'react-modal';
+import ReactDOM from 'react-dom';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 class ChannelIndex extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    const appElement = document.getElementById('root');
+    Modal.setAppElement(appElement);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
 
   componentDidMount() {
     this.props.fetchChannels();
@@ -14,8 +54,31 @@ class ChannelIndex extends React.Component {
 
     return (
     <div className='channel-index'>
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        >
+        <h2 ref={this.subtitle}>Hello</h2>
+        <button onClick={this.closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
       <div className='channels'>
-        <h2 className='channel-index-header'>CHANNELS <span>({channels.length})</span></h2>
+        <div className='channels-header'>
+          <h2 className='channel-index-header'>CHANNELS <span>({channels.length})</span></h2>
+          <button className='new-channel-button' onClick={this.openModal}>
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+          </button>
+        </div>
         {channels.map(channel => (
           <ChannelIndexItem channel={channel} key={channel.id}/>
         ))}
