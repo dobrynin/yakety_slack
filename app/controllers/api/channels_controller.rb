1361523f7b036ba1712397581184b5fc44f07689
App Academy Student <student@appacademy.io>
2017-05-20 21:24:00 -0700

    Updates channel controller to subscribe channel creator

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -2,6 +2,10 @@ class Api::ChannelsController < ApplicationController
   def create
     @channel = Channel.new(channel_params)
     if @channel.save
+      Subscription.create!(
+        user_id: @channel.moderator_id,
+        channel_id: @channel.id
+      )
       render @channel
     else
       render json: @channel.errors.full_messages, status: 422
